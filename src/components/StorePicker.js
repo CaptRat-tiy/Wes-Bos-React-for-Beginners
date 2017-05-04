13a6847db8c7a8ef4f205baf007147c10c9189cc
Patrick Logan <patricklogan@Patricks-iMac-2.local>
2017-05-04 10:04:46 -0600

    Add routing this.storeInput

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+  goToStore(event) {
+    event.preventDefault();
+    console.log("You changed the URL");
+    //first grab the text from the store's box
+    // then transition from / to /store/:storeID
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
