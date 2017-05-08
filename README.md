3bea5f672c4d47f8c50bd8f1000f86ddf1b535ce
Patrick Logan <jpatricklogan@gmail.com>
2017-05-07 23:17:03 -0600

    following.  Video 14 completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is Wes Bos' react tutorial, where I will code-along and build a Fish Restaurant "Catch of the Day"
+
+start with npm start to start (internal) webpack and load developer server
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,22 +2,30 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+
   goToStore(event) {
     event.preventDefault();
     console.log("You changed the URL");
     //first grab the text from the store's box
+    const storeID = this.storeInput.value;
+    console.log(`Going to ${storeID}`)
     // then transition from / to /store/:storeID
+    this.context.router.transitionTo(`/store/${storeID}`);
   }
 
   render(){
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
