b129000708dd7e369c1f16580fba5b96777a64fa
Patrick Logan <jpatricklogan@gmail.com>
2017-05-08 22:58:17 -0600

    finished chapter 16

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,19 +3,20 @@ import { formatPrice} from '../helpers';
 
 class Fish extends React.Component {
   render() {
-    const { details } = this.props;
+    const { details, index } = this.props;
+
     const isAvailable = details.status === 'available';
     const buttonText = isAvailable ? 'Add to Order' : 'Sold Out!';
 
     return (
       <li className="menu-fish">
-        <img src={details.image} alt={details.image}/>
+        <img src={details.image} alt={details.name}/>
         <h3 className="fish-name">
           {details.name}
           <span className="price">{formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button onClick={() => this.props.addToOrder('fish-1')} disabled={!isAvailable}>{buttonText}</button>
+        <button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
       </li>
     )
   }
